c31bbd49da29fe38465551df17116b213d7bef97
Loïc Paulevé <loic@inzenet.org>
2010-08-28 18:40:11 +0200

    remove ignored files

--- localsettings.py
+++
@@ -1,9 +0,0 @@
-
-DJANGO_PATH = "/home/panard/rech/jebif/lib/Django-1.2.1"
-ADMIN_TEMPLATE_DIR = DJANGO_PATH + "django/contrib/admin/templates"
-ADMIN_MEDIA_ROOT = DJANGO_PATH + "django/contrib/admin/media"
-ROOT_URL = ''
-
-# $ python -c 'import random; print "".join([random.choice("abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)") for i in range(50)])'
-SECRET_KEY = '+d&339susn9+(l&htyfmii316&*&m^8kpqm$=9zrcnd^ii5x8s'
-
